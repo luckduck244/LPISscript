bc6d90f92c374acdc41b77fe8a19f9279403f7de
Max <Max@Maxs-MacBook-Air.local>
2018-03-14 11:10:10 +0100

    asdf

--- content3.js
+++ content3.js
@@ -1,5 +1,5 @@
 addCheckboxes();
-
+submitAdd();
 
 function addCheckboxes()
 {
@@ -9,7 +9,7 @@ function addCheckboxes()
         var element = document.createElement("input");
         element.setAttribute("value", "auto login");
         element.setAttribute("type", "checkbox");
-        element.setAttribute("id", "al"+i);
+        element.setAttribute("id", "cb"+i);
         element.onclick = checkBox;
   //      element.submitForm = td[i];
         td[i].parentNode.appendChild(element);
@@ -31,4 +31,45 @@ function checkBox()
     }
 }
 
-// function for submitbutton
+function submitAdd() {
+
+    var element = document.createElement("input");
+    element.setAttribute("type", "button");
+    element.setAttribute("value", "Submit");
+    element.onclick = submitClick;
+    document.body.appendChild(element);
+
+}
+
+
+function submitClick() {
+
+    var checkBoxes = document.querySelectorAll("[id^=cb]");
+    var rowsClicked = [];
+
+for (var count = 0; checkBoxes.length > count; count++){
+
+    if (checkBoxes[count].checked) {
+        rowsClicked.push(checkBoxes[count].parentNode.parentNode);
+    }
+
+
+}
+    console.log(rowsClicked);
+
+
+/*
+
+chrome.storage.local.set({'row': banana}, function() {
+    // banana save notification
+    // console.log('Banana saved');
+});
+
+chrome.storage.local.get('color', function(result) {
+    var bananaColor = result.color;
+    window.alert(bananaColor);
+});
+
+*/
+
+}
